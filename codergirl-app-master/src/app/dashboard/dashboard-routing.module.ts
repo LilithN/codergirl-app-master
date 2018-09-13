2d6b2325f9442a33fdcdf73ce35aa0ca70c436a4
Samantha Goodson <Shad0wech00@gmail.com>
2018-09-12 19:12:17 -0500

    added module, components, and some routes

--- codergirl-app-master/src/app/dashboard/dashboard-routing.module.ts
+++ codergirl-app-master/src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: VideoDashboardComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
