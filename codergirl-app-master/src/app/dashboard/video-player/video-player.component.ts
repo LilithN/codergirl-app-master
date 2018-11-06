edaea4a16c2505bac2fb6465d208d1f6d78bb6a5
Samantha Goodson <Shad0wech00@gmail.com>
2018-11-05 19:44:50 -0600

    video list connected to video player via input and some styling

--- codergirl-app-master/src/app/dashboard/video-player/video-player.component.ts
+++ codergirl-app-master/src/app/dashboard/video-player/video-player.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Videos } from '../../video-data.service';
+
+
 
 @Component({
   selector: 'cg-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.css']
 })
-export class VideoPlayerComponent implements OnInit {
+export class VideoPlayerComponent {
+  @Input() video: Videos;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
